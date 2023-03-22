3c1c316b84eb36ce8a99d7d9fa3b9da9b7987e23
gbriedis <46389671+gbriedis@users.noreply.github.com>
2023-03-22 22:22:45 +0000

    Updated our mission

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
         customOrange: '#C37C28',
         customRed: '#C32828'
       },
+      dropShadow: {
+        'green': ['rgb(255,153,0) 0px 0px 1px','rgba(249,164,0, 0.6) 0px 0px 5px', 'rgba(249,164,0, 0.4) 0px 5px 4px'],
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ],
+      },
     },
   },
   plugins: [],
